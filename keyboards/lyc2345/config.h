b46c43998d9b888dd113b8c73e2042ee4b313694
Stan Liu <lyc2345@gmail.com>
2025-06-12 21:30:52 +0800

    feat: Add config.h

---
+++ keyboards/lyc2345/config.h
@@ -0,0 +1,40 @@
+/* Copyright 2023 Finalkey
+ * Copyright 2023 LiWenLiu <https://github.com/LiuLiuQMK>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* Define less important options */
+/*
+ * Feature disable options
+ * These options are also useful to firmware size reduction.
+ */
+
+#define MATRIX_UNSELECT_DRIVE_HIGH
+#define CORTEX_ENABLE_WFI_IDLE          FALSE
+
+/* Ensure we jump to bootloader if the RESET keycode was pressed */
+#define EARLY_INIT_PERFORM_BOOTLOADER_JUMP TRUE
+
+#define DEBOUNCE 5
+
+#ifndef NOP_FUDGE
+#define NOP_FUDGE 0.4
+#endif
